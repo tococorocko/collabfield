fda62e214a5ae23b66cc29f3ca767e0211da2c96
Lucca Kaiser <luccakaiser@gmail.com>
2019-08-10 21:54:44 +0200

    Split home page template's content into partials

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,14 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def hobby
+    @posts = Post.where(category="Hobby")
+  end
+  def study
+
+  end
+  def team
+
+  end
 end
