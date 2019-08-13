3456751136eede6336a2b658439c81008fa35498
Lucca Kaiser <luccakaiser@gmail.com>
2019-08-13 21:46:11 +0200

    update deprecated tests

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe PostsHelper, :type => :helper do
 
   context '#create_new_post_partial_path' do
     it "returns a signed_in partial's path" do
-      helper.stub(:user_signed_in?).and_return(true)
+      allow(helper).to receive(:user_signed_in?).and_return(true)
       expect(helper.create_new_post_partial_path). to (
         eq 'posts/branch/create_new_post/signed_in'
       )
     end
 
     it "returns a signed_in partial's path" do
-      helper.stub(:user_signed_in?).and_return(false)
+      allow(helper).to receive(:user_signed_in?).and_return(false)
       expect(helper.create_new_post_partial_path). to (
         eq 'posts/branch/create_new_post/not_signed_in'
       )
@@ -52,14 +52,14 @@ RSpec.describe PostsHelper, :type => :helper do
 
   context '#post_format_partial_path' do
     it "returns a home_page partial's path" do
-      helper.stub(:current_page?).and_return(true)
+      allow(helper).to receive(:current_page?).and_return(true)
       expect(helper.post_format_partial_path).to (
         eq 'posts/post/home_page'
       )
     end
 
     it "returns a branch_page partial's path" do
-      helper.stub(:current_page?).and_return(false)
+      allow(helper).to receive(:current_page?).and_return(false)
       expect(helper.post_format_partial_path).to (
         eq 'posts/post/branch_page'
       )
