c7f0c75092792aba3004a493469abad2e5b299d8
Lucca Kaiser <luccakaiser@gmail.com>
2019-08-22 21:37:55 +0200

    Create a UI to create new posts- Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  # used as before_action in child-controllers
+  def redirect_if_not_signed_in
+    redirect_to root_path if !user_signed_in?
+  end
+
+  def redirect_if_signed_in
+    redirect_to root_path if user_signed_in?
+  end
 end
